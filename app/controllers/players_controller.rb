88c5d72aa20f18b58f0896f3390a1a4d64e89e8e
gameplan1975 <gameplan1975@gmail.com>
2020-07-25 13:35:14 +0900

    playerモデルの分離とcrud作成

--- app/controllers/players_controller.rb
+++ app/controllers/players_controller.rb
@@ -1,19 +1,60 @@
 class PlayersController < ApplicationController
-  def new
-  end
+  before_action :set_player, only: [:show, :edit, :update, :destroy]
+  before_action :default_date_setting, only: [:create]
 
-  def edit
+  def index
+    @players = Player.all
+  end
+    
+  def new
+    @player = Player.new
   end
 
   def show
+    @player = Player.find_by(id: params[:id])
   end
+    
+  def edit
+  end
+    
+  def create
+    @player = Player.new(player_params)
+    #この辺りをストロングパラメータにしたい
+    @player.user_id = current_user.id
+    @player.game = 0
+    @player.correct = 0
+    @player.total_time = 0
 
+    if @player.save
+      redirect_to players_path
+    else
+      render :new
+    end
+  end
+    
   def update
+    if @player.update(player_params)
+      redirect_to players_path
+    else
+      render :edit
+    end
   end
-
-  def index
-  end
-
+    
   def destroy
+    @player.destroy
+    redirect_to players_path
   end
+    
+  private
+  def set_player
+    @player = Player.find(params[:id])
+  end
+  
+  def player_params
+    params.require(:player).permit(:name, :user_id, :favorite_category, :game, :correct, :total_time, :comment)
+  end
+
+  def default_date_setting
+    #できれば初期設定はここでやりたい
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-  get 'players/new'
-  get 'players/edit'
-  get 'players/show'
-  get 'players/update'
-  get 'players/index'
-  get 'players/destroy'
   get 'homes/top'
   resources :genres
   resources :questions
+  resources :players
   root "homes#top"
   devise_for :users
   if Rails.env.development?
