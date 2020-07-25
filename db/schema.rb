c5798340335006af9b8f0d6d42978f96986c924e
gameplan1975 <gameplan1975@gmail.com>
2020-07-25 16:03:48 +0900

    ページネーションと不要カラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_25_024743) do
+ActiveRecord::Schema.define(version: 2020_07_25_065558) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,7 +37,6 @@ ActiveRecord::Schema.define(version: 2020_07_25_024743) do
   create_table "questions", force: :cascade do |t|
     t.string "name"
     t.string "memo"
-    t.date "accrual_date"
     t.boolean "playable"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -57,12 +56,7 @@ ActiveRecord::Schema.define(version: 2020_07_25_024743) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "favorite_category"
-    t.integer "game"
-    t.integer "correct"
     t.string "name"
-    t.string "comment"
-    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
